2ccf5b8b056348fbef52211c7efd5fc971a68d38
iplatos <platonoff9168@gmail.com>
2024-09-07 06:51:25 +0300

    feat: add react context

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
 import '../src/scss/app2.scss'
 import { Header } from './components/header'
 
-import { useEffect, useState } from 'react'
-import { PizzaPropsType } from './components/pizzaBlock'
+import { createContext, useState } from 'react'
 import { Home } from './pages/home'
 import { Route, Routes } from 'react-router-dom'
 import { NotFound } from './pages/notFoung'
 import { Cart } from './pages/cart'
-//v 10
+import React from 'react'
+//v 11
 
+export const SearchContext = createContext<{
+  searchValue: string
+  setSearchValue: React.Dispatch<React.SetStateAction<string>>
+}>({
+  searchValue: '',
+  setSearchValue: () => {},
+})
 function App() {
   const [modalSortOpen, setModalSortOpen] = useState(false)
   const [isLoading, setIsLoading] = useState(true)
+  const [searchValue, setSearchValue] = useState<string>('')
 
   const modalClose = () => {
     if (modalSortOpen) {
@@ -22,25 +30,27 @@ function App() {
   return (
     <>
       <div onClick={modalClose} className="wrapper">
-        <Header />
-        <div className="content">
-          <Routes>
-            {' '}
-            <Route
-              path="/"
-              element={
-                <Home
-                  setIsLoading={setIsLoading}
-                  modalSortOpen={modalSortOpen}
-                  setModalSortOpen={setModalSortOpen}
-                  isLoading={isLoading}
-                />
-              }
-            />
-            <Route path={'/cart'} element={<Cart />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </div>
+        <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+          <Header />
+          <div className="content">
+            <Routes>
+              {' '}
+              <Route
+                path="/"
+                element={
+                  <Home
+                    setIsLoading={setIsLoading}
+                    modalSortOpen={modalSortOpen}
+                    setModalSortOpen={setModalSortOpen}
+                    isLoading={isLoading}
+                  />
+                }
+              />
+              <Route path={'/cart'} element={<Cart />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </SearchContext.Provider>
       </div>
     </>
   )
