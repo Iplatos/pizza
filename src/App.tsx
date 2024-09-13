abba031aea1937f21f164b9f243fb4cfb1601e5b
iplatos <platonoff9168@gmail.com>
2024-09-13 22:16:09 +0300

    refactor: modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import '../src/scss/app2.scss'
 import { Header } from './components/header'
 
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { Home } from './pages/home'
 import { Route, Routes } from 'react-router-dom'
 import { NotFound } from './pages/notFound'
-import { Cart } from './pages/cart'
+
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from './store/store'
-import { isModalSortOpen } from './store/appSlice'
+import { Cart } from './pages/cart/cart'
 //v 16
 
 export const SearchContext = createContext<{
@@ -20,20 +18,20 @@ export const SearchContext = createContext<{
   setSearchValue: () => {},
 })
 function App() {
-  const modalSortOpen = useSelector((state: RootState) => state.AppReducer.modalSortOpen)
-
+  // const modalSortOpen = useSelector((state: RootState) => state.AppReducer.modalSortOpen)
   const [searchValue, setSearchValue] = useState<string>('')
-  const dispatch = useDispatch()
+  // const dispatch = useDispatch()
 
-  const modalClose = () => {
-    if (modalSortOpen) {
-      dispatch(isModalSortOpen(false))
-    }
-  }
+  // const modalClose = () => {
+  //   if (modalSortOpen) {
+  //     dispatch(isModalSortOpen(false))
+  //   }
+  // }
 
+  useEffect(() => {}, [])
   return (
     <>
-      <div onClick={modalClose} className="wrapper">
+      <div className="wrapper">
         <SearchContext.Provider value={{ searchValue, setSearchValue }}>
           <Header />
 
